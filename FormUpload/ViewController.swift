4ea2d794a3da25971f29a916c6002305d7918caa
LaurenWhite <lauren5826@gmail.com>
2019-09-19 22:44:50 -0400

    Connected UI to view controller file

--- FormUpload/ViewController.swift
+++ FormUpload/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK - UI Outlets
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var ageTextField: UITextField!
+    @IBOutlet var majorTextField: UITextField!
+    @IBOutlet var uploadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        uploadButton.isEnabled = false
+    }
+    
+    func toggleUploadButtonAvailablity() {
+        uploadButton.isEnabled =
+            (nameTextField.text != "") &&
+            (ageTextField.text != "") &&
+            (majorTextField.text != "")
     }
 
-
+    @IBAction func nameTextFieldChanged(_ sender: Any) {
+        toggleUploadButtonAvailablity()
+    }
+    
+    @IBAction func ageTextFieldChanged(_ sender: Any) {
+        toggleUploadButtonAvailablity()
+    }
+    
+    @IBAction func majorTextFieldChanged(_ sender: Any) {
+        toggleUploadButtonAvailablity()
+    }
+    
+    @IBAction func uploadButtonPressed(_ sender: Any) {
+        // Upload answers to dropbox
+    }
 }
 
